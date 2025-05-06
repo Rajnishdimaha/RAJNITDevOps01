780f8798db1c5944dab52ede418d771661717567
Rajnishdimaha <rajnishkumardimaha@gmail.com>
2025-05-06 15:47:25 +0530

    My new project

---
+++ Gitprog03Demo/src/com/nt/main/Calculation.java
@@ -0,0 +1,16 @@
+package com.nt.main;
+
+public class Calculation {
+	
+	public static int sum(int x,int y) {
+		
+		return x+y;
+	}
+	
+	
+    public static int sub(int x,int y) {
+		
+		return x-y;
+	}
+
+}
---
+++ Gitprog03Demo/src/com/nt/main/TestDemo3.java
@@ -0,0 +1,13 @@
+package com.nt.main;
+
+public class TestDemo3 {
+	
+	public static void main(String[] args) {
+		int sum = Calculation.sum(100, 200);
+		System.out.println("Sum :"+sum);
+		System.out.println("=================================");
+		int sub = Calculation.sub(200, 100);
+		System.out.println("Sub :"+sub);
+	}
+
+}
